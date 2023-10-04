4a42109eb3a548b74c392cebcc5160a097ee3ed2
Leena Rajan Katkar <110556075+fhisheruser@users.noreply.github.com>
2023-10-04 17:05:01 +0530

    Add files via upload

---
+++ CardData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c1",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1175 + order placed from here recently",
+        price: 350,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c2",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 25,
+        rating: "3.9",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c3",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed from here recently",
+        price: 70,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c4",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 70,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c5",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1050 + order placed from here recently",
+        price: 210,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c6",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1100 + order placed from here recently",
+        price: 100,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c7",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 300,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c8",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 100,
+        rating: "3.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        Lang: "English",
+        imgdata: "https://thumbs.dreamstime.com/z/teaching-helping-study-course-team-young-teacher-tutor-helps-students-to-learning-classroom-homework-test-exam-168623338.jpg",
+        address: "c9",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2525 + order placed from here recently",
+        price: 100,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
